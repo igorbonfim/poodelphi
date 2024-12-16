831cdb521ca0990d502c7f28fba91d9e58a7bfb7
Igor Bonfim <igorbonfim88@gmail.com>
2024-12-15 23:50:55 -0300

    Classes seladas e método final

--- Classe.Animal.pas
+++ Classe.Animal.pas
@@ -15,6 +15,14 @@ type
     function Voz: String; override;
   end;
 
+  TDinossauro = class(TAnimal)
+    function Voz: String; override; final; // final - indica que o método não pode mais ser sobreescrito
+  end;
+
+  TTiranossauro = class sealed (TDinossauro) // sealed - indica que a classe não pode ser mais herdada ou especializada
+
+  end;
+
 implementation
 
 { TCachorro }
@@ -29,6 +37,13 @@ end;
 function TGato.Voz: String;
 begin
   Result := 'Miau';
+end;
+
+{ TDinossauro }
+
+function TDinossauro.Voz: String;
+begin
+  Result := 'rhaaaa';
 end;
 
 end.
