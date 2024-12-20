ab6a2998ac3defc97b883c44bad91d2fbb36d23d
Igor Bonfim <igorbonfim88@gmail.com>
2024-12-19 23:21:40 -0300

    Adicionando propriedades ao form

--- Form.Padrao.pas
+++ Form.Padrao.pas
@@ -5,12 +5,10 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
-  FMX.Controls.Presentation;
+  FMX.Controls.Presentation, FMX.Layouts;
 
 type
   TFormPadrao = class(TForm)
-    Panel1: TPanel;
-    Panel2: TPanel;
     Button1: TButton;
     Button2: TButton;
     Button3: TButton;
@@ -18,11 +16,21 @@ type
     Label1: TLabel;
     Button5: TButton;
     StyleBook1: TStyleBook;
+    Label2: TLabel;
+    Layout1: TLayout;
+    Layout2: TLayout;
+    Layout3: TLayout;
     procedure Button3Click(Sender: TObject);
+    procedure FormMouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Single);
   private
+    FClicks: Integer;
+    procedure SetClicks(const Value: Integer);
+    function GetClicks: Integer;
     { Private declarations }
   public
     { Public declarations }
+    property Clicks: Integer read GetClicks write SetClicks;
   end;
 
 var
@@ -35,6 +43,23 @@ implementation
 procedure TFormPadrao.Button3Click(Sender: TObject);
 begin
   ShowMessage('Inserido com sucesso');
+end;
+
+procedure TFormPadrao.FormMouseDown(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: Single);
+begin
+  SetClicks(1);
+end;
+
+function TFormPadrao.GetClicks: Integer;
+begin
+  Result := FClicks;
+end;
+
+procedure TFormPadrao.SetClicks(const Value: Integer);
+begin
+  FClicks := FClicks + Value;
+  Label2.Text := IntToStr(FClicks) + ' Cliques';
 end;
 
 end.
